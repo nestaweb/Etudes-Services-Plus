c7cf1983959da5ba48f07ce1d492e3f024df4a2f
nesta.web <nesta.web.dev@gmail.com>
2023-11-09 12:13:38 +0100

    pdf

---
+++ src/components/Contact.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+	// import ImageMultiply from './ImageMultiply.vue'
+</script>
+
+<template>
+	<section class="section section--small--padding">
+		<div class="container">
+			<div class="contact">
+				
+			</div>
+		</div>
+	</section>
+</template>
